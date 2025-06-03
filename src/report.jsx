fed75c72c007d437d27c9f78bfb4e2e527495379
tae <67130873@dpu.ac.th>
2025-06-03 14:42:55 +0700

    setup gh-pages

--- src/report.jsx
+++ src/report.jsx
@@ -62,7 +62,7 @@ export default class report extends Component {
         <Navbar bg="primary" variant="dark">
           <Container>
             <Navbar.Brand href="#">ค้นหาเลขไปรษณีย์</Navbar.Brand>
-            <div>{/* Name */}</div>
+            <div>{" 57130873 นายมรกต พรรณพิจิตร "}</div>
           </Container>
         </Navbar>
         <Container>
